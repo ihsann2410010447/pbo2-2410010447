07c1d6c196864db169d96ac9a55955b8a2bf1be7
Akbar Ihsan Nugraha <ihsan200630@gmail.com>
2026-09-25 17:28:53 +0800

    p02:perpustakaan-mini

---
+++ p02-perpustakaan-mini/src/main/java/id/ac/uniska/pbo2/p02/Anggota.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p02;
+
+/**
+ *
+ * @author User
+ */
+public record Anggota(String npm, String nama) {
+}
---
+++ p02-perpustakaan-mini/src/main/java/id/ac/uniska/pbo2/p02/StatusKoleksi.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package id.ac.uniska.pbo2.p02;
+
+/**
+ *
+ * @author User
+ */
+public enum StatusKoleksi {
+    TERSEDIA,
+    DIPINJAM
+    
+}
